26bc7900ee37e88f1f8015c66d90548cc75f64f7
Vishnu Ks <hackerkid@vishnuks.com>
2016-01-20 18:01:51 +0530

    simple table implemented with vector

--- token.cpp
+++ token.cpp
@@ -1,14 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 
-enum states { TEXT, SINGLE_QUOTE, SINGLE_SLASH, SINGLE_LINE_COMMENT, MULTI_COMMENT, MULTI_LINE_COMMENT, STAR_FOUND, SINGLE_SINGLE_QUOTE};
+enum states { NEW_WORD, NUMBER, IDENTIFIER, SINGLE_QUOTE, SINGLE_SLASH, SINGLE_LINE_COMMENT, MULTI_COMMENT, MULTI_LINE_COMMENT, STAR_FOUND, SINGLE_SINGLE_QUOTE, DATA_TYPE, KEY_WORD};
 using namespace std;
 
-ifstream ifwords("keywords");
+typedef struct simple_table
+{
+	string identifer;
+	string data_type;
+	string scope;
+} simple_table;
 
-//read the file from the file
+vector <string> number_arr;
+vector <string> keyword_arr;
+vector <string> identifer_arr;
+vector <simple_table> simple_table_arr;
+
+
+bool region_opening(char c)
+{
+	if(c == '{' or c == '(') {
+		return true;
+	}
+
+	return false;
+
+}
+bool region_closing(char c)
+{
+	if(c == '}' or c == ')') {
+		return true;
+	}
+
+	return false;
+
+}
+int is_data_type(string x)
+{
+
+	string types[] = {"byte", "short", "int", "long", "float", "double", "boolean", "char"};
+	
+	for (int i = 0; i < types[i].size(); i++) {
+		if(types[i] == x) {
+			return 1;
+		}
+	}
+
+	return false;
+}
+string braceid_to_string( int x)
+{
+	string res;
+	int temp;
+	char c;
+	temp = x;
+
+	res = "";
+	if(x == 0) {
+		res = "0";
+	}
+	while(x) {
+		c = x % 10 + '0';
+		res = c + res; 
+		x = x / 10;
+	}
+	//string ans = id + " { inside region " + res + " }";
+	return res;
+}
 string read_file(const char * name)
 {
 	std::ifstream fcin(name);
@@ -23,24 +84,23 @@ string read_file(const char * name)
 bool check_for_keywords(string word)
 {	
 	string keyword;
-	cout << word << endl;
+	ifstream ifwords("keywords");
 	while(ifwords >> keyword) {
 		if(keyword == word) {
-			cout << word << endl;
  			return true;
 		}
 	}
 
 	return false;
 }
-//check 
 
 int check_for_tokens(string x)
 {
-	if(check_for_keywords(x))
-		return true;
+	if(check_for_keywords(x)) {
+		return 1;
+	}
 
-	return TEXT;
+	return 0;
 }
 
 
@@ -48,14 +108,42 @@ void parser(string file)
 {	
 	int state;
 	char c;
+	int brace_count;
 	string word;
-	state = TEXT;
+	string previous_word;
+
+	brace_count = 0;
+
+	state = NEW_WORD;
 
 	for(int i = 0; i < file.length(); i++) {
 		c = file[i];
 	
 		switch(state) {
-			case TEXT:
+			// new word is just read
+
+			case NEW_WORD:
+			// hell yaeh identifer or keyword
+				if(isalpha(c) or c == '_') {
+					state = IDENTIFIER;
+					word = word + c;
+					break;
+
+				}
+			//its a digit
+
+				if(isdigit(c)) {
+					state = NUMBER;
+					word = word + c;
+					break;
+				}
+
+				if(region_opening(c)) {
+					brace_count++;
+				}
+				if(region_closing(c)) {
+					brace_count--;
+				}
 
 				switch(c) {
 					case '/':
@@ -70,13 +158,94 @@ void parser(string file)
 						state = SINGLE_SINGLE_QUOTE;
 						break;
 					case ' ':
-						state = check_for_tokens(word);
+					case '\n':
+					case ';':
+					case '(':
+					case ')':
+					case '[':
+					case ']':
+					case '*':
+					case '+':
+					case '=':
+					case '-':
+					case '\t':
+					case '{':
+					case '}':
+					case '<' :
+						previous_word = word;
 						word = "";
-					default:
-						word = word + c;
+						state = NEW_WORD;
+						break;
+
 						
 				}
 
+				break;
+
+			case IDENTIFIER:
+
+				if(region_opening(c)) {
+					brace_count++;
+				}
+				if(region_closing(c)) {
+					brace_count--;
+				}
+
+				switch(c) {
+					case ' ':
+					case '\n':
+					case ';':
+					case '(':
+					case ')':
+					case '[':
+					case ']':
+					case '*':
+					case '+':
+					case '=':
+					case '-':
+					case '\t':
+					case '{':
+					case '}':
+					case '<' :
+						if(check_for_tokens(word)) {
+							keyword_arr.push_back(word);
+						}
+						else {
+							if(is_data_type(previous_word)) {
+								//string id = braceid_to_string(word, brace_count);
+								simple_table temp;
+								temp.data_type = previous_word;
+								temp.scope = braceid_to_string(brace_count);
+								temp.identifer = word;
+
+								simple_table_arr.push_back(temp);
+
+								//cout << id << " data " << previous_word << endl;
+							}
+							identifer_arr.push_back(word);
+						}
+
+						previous_word = word;
+						word = "";
+						state = NEW_WORD;
+						break;
+
+					default:
+						word = word + c;
+						state = IDENTIFIER;
+						break;
+				}
+				break;
+
+			case NUMBER:
+				if(isdigit(c)) {
+					word = word + c;
+					break;
+				}
+				number_arr.push_back(word);
+				previous_word = word;
+				word = "";
+				state = NEW_WORD;
 				break;
 
 			case SINGLE_SLASH:
@@ -95,7 +264,8 @@ void parser(string file)
 			case SINGLE_LINE_COMMENT:
 				switch(c) {
 					case '\n':
-						state = TEXT;
+						state = NEW_WORD;
+						previous_word = word;
 						word = "";
 						break;
 				}
@@ -113,7 +283,8 @@ void parser(string file)
 			case STAR_FOUND:
 				switch(c) {
 					case '/':
-						state = TEXT;
+						state = NEW_WORD;
+						previous_word = word;
 						word = "";
 						break;
 				}
@@ -122,7 +293,8 @@ void parser(string file)
 			case SINGLE_QUOTE:
 				switch(c) {
 					case '"':
-					state = TEXT;
+					state = NEW_WORD;
+					previous_word = word;
 					word = "";
 					break;
 				}
@@ -131,7 +303,8 @@ void parser(string file)
 			case SINGLE_SINGLE_QUOTE:
 				switch(c) {
 					case '\'':
-						state = TEXT;
+						state = NEW_WORD;
+						previous_word = word;
 						word = "";
 						break;
 				}
@@ -140,13 +313,69 @@ void parser(string file)
 	}
 } 
 
+void print_simple_table()
+{
+	cout << "SIMPLE TABLE\n";
+	cout << "-----------\n\n";
+
+	for (int i = 0; i < simple_table_arr.size(); i++) {
+		cout << simple_table_arr[i].identifer << " " << simple_table_arr[i].data_type << " " << simple_table_arr[i].scope << endl;
+	}
+
+	cout << "\n\n";
+}
+	
+	
+
+void print_identifiers()
+{	
+	cout << "IDENTIFIERS\n";
+	cout << "-----------\n\n";
+
+	for (int i = 0; i < identifer_arr.size(); i++) {
+		cout << identifer_arr[i] << endl;
+	}
+	cout << "\n";
+}
+
+void print_keywords()
+{	
+	cout << "KEYWORDS\n";
+	cout << "-----------\n\n";
+
+	for (int i = 0; i < keyword_arr.size(); i++) {
+		cout << keyword_arr[i] << endl;
+	}
+	cout << "\n";
+
+}
+
+void print_numbers()
+{	
+	cout << "NUMBERS\n";
+	cout << "-----------\n\n";
+
+	for (int i = 0; i < number_arr.size(); i++) {
+		cout << number_arr[i] << endl;
+	}
+	cout << "\n";
+
+}
+
 
 int main()
 {
 	string file;
 
 	file = read_file("input");
+
+	
 	parser(file);
+
+	print_identifiers();
+	print_keywords();
+	print_numbers();
+	print_simple_table();
 
 }
 
