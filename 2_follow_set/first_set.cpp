320a31b8ec1c6811dc7c649ef77f6c4e2e8f0b51
Vishnu Ks <hackerkid@vishnuks.com>
2016-02-17 17:10:52 +0530

    c++ added

--- 2_follow_set/first_set.cpp
+++ 2_follow_set/first_set.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #define m map <string,  vector <string> > 
 #define ARROW "→"
-#define epsilon "ε"
+#define EPSILON "ε"
 using namespace std;
 
 
@@ -27,35 +27,51 @@ m follow_table;
 bool terminal(string c)
 {
 
-	return !(mp.count(c) != 0);
+	return (mp.count(c) == 0);
 }
 
-vector <string> create_follow_table(string c)
-{
+/*
+void create_follow_table(string c)
+{	
+
+	cout << "c " << c << endl;
 
 	if(terminal(c)) {
 		vector <string> temp;
-		temp.push_back(c);
-		return temp;
+		//temp.push_back(c);
+		//return temp;
 	}
 
 	if(follow_table.count(c)) {
-		return follow_table[c];
+		//return follow_table[c];
 	}
-		
+	
+	//iterate through all the exapnsions
+
+	/*
 	for (int i = 0; i < mp[c].size(); i++) {
-		vector <string> expansions = mp[c];
-		bool epsilon_found = true;
+		string expansions = mp[c][i];
+		bool EPSILON_found = true;
 		
-		for (int j = 0; j < expansions.size() and epsilon_found; j++) {
-			vector <string> exp_follows = create_follow_table(expansions[j]);
-			epsilon_found = false;
+		cout << "expansions " << expansions << endl; 
+
+		//itereate through all the chars in an expansion
+
+		/*
+		for (int j = 0; j < expansions.length() and EPSILON_found; j++) {
+			string c_to_st;
+			char c;
+			c = expansions[j];
+			c_to_st = c;
+			
+			vector <string> exp_follows = create_follow_table(c_to_st);
+			EPSILON_found = false;
 
 			for (int k = 0; k < exp_follows.size(); k++) {
-				if(exp_follows[k] == epsilon) {
-					epsilon_found = true;
+				if(exp_follows[k] == EPSILON) {
+					EPSILON_found = true;
 					if(k == 0) {
-						follow_table[c].push_back(epsilon);
+						follow_table[c].push_back(EPSILON);
 					}
 				}
 				else {
@@ -64,7 +80,60 @@ vector <string> create_follow_table(string c)
 			}
 		}
 	}
-	
+
+		
+
+}
+*/
+vector <string>  create(string c)
+{
+	vector <string> temp;
+	cout << " the input is " << c << endl;
+	if(terminal(c)) {
+		cout << "hell yeah" << c << endl;
+		follow_table[c].push_back(c);
+		temp.push_back(c);
+		return temp;
+	}
+
+	cout << "see the productions" << endl;
+
+	for (int i = 0; i < mp[c].size(); i++) {
+		if(mp[c][i] == EPSILON) {
+			follow_table[c].push_back(EPSILON);
+			temp.push_back(EPSILON);
+			return temp;
+		}
+		else {
+			if(terminal(mp[c][i])) {
+
+				follow_table[c].push_back(mp[c][i]);
+				temp.push_back(mp[c][i]);
+				return temp;
+			}
+			else {
+				cout << mp[c][i] << 
+				bool epsilon_found = false;
+				
+				for (int k = 0; k < mp[c][i].length() and !epsilon_found; k++) { 
+					char single_element;
+
+					single_element = mp[c][i][k];
+					string letter(1,single_element );
+					vector <string> result = create(letter);
+
+					for (int l = 0; l < result.size(); l++) {
+						follow_table[c].push_back(result[l]);
+						temp.push_back(result[l]);
+					}
+
+				}
+			}
+		}
+
+	}
+
+	return temp;
 
 }
 void process_input()
@@ -109,7 +178,7 @@ int main()
 
 	print(mp);
 
-	create_follow_table();
+	create("E");
 	
 }
 
